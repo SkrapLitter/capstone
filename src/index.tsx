3bf1fc42f42ee6327ae6849cd3e98bec2dab3f9a
nickcarr51 <nick.carr84@gmail.com>
2020-08-28 11:20:47 -0400

    server and run in browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { render } from 'react-dom'
-
-const App = () => {
-  return <h1>Hello World</h1>
-}
+import App from './app'
 
 render(
   <Router>
