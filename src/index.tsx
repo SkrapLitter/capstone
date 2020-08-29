3a372b4dbc317e967ee5dd0a2af8fcf4a8af4252
jlam609 <jlam609@gmail.com>
2020-08-28 21:43:14 -0400

    fixed some stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { render } from 'react-dom'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from 'react-dom';
 
 const App = () => {
-  return <h1>Hello World</h1>
-}
+  return <h1>Hello World</h1>;
+};
 
 render(
   <Router>
@@ -12,4 +12,4 @@ render(
   </Router>,
   document.getElementById('root'),
   () => console.log('rendered')
-)
+);
