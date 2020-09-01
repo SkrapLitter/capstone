4f22d3cee23c43606a810654925e8db0e52f09a0
jlam609 <jlam609@gmail.com>
2020-08-31 22:45:21 -0400

    changes

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
 const { models } = require('./models');
 const db = require('./db');
 
-const { User, Job } = models;
+const { User } = models;
 
 const sync = async () => {
   try {
-    const user = await User.create({
+    await User.create({
       username: 'admin@fullstack.com',
       password: 'password',
       firstName: 'Default',
@@ -13,16 +13,6 @@ const sync = async () => {
       image:
         'https://thumbs.dreamstime.com/b/red-admin-sign-pc-laptop-vector-illustration-administrator-icon-screen-controller-man-system-box-88756468.jpg',
       clearance: 5,
-    });
-    console.log(user.validPassword('password'));
-    await Job.create({
-      name: 'McCarren Park',
-      status: 'paid',
-      price: 20,
-      city: 'Brooklyn',
-      state: 'New York',
-      address: 'Bedford Ave and Lorimer St',
-      userId: user.id,
     });
   } catch (e) {
     console.error(e);
