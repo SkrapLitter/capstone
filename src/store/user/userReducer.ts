9ebddd7e3f27cf78de22af592534b2baf172627a
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-09-01 22:04:23 -0400

    Added validation error

--- src/store/user/userReducer.ts
+++ src/store/user/userReducer.ts
@@ -7,16 +7,20 @@ const defaultUser: User = {
   lastName: 'McNotLoggedIn',
   image: '',
   clearance: 0,
+  error: '',
 };
 
 const userReducer = (state: User = defaultUser, action): User => {
-  const { user, type } = action;
+  const { user, type, error } = action;
   switch (type) {
     case 'LOGIN_SUCCESS':
     case 'CREATE_ACCOUNT':
       return user;
     case 'LOGIN_FAIL':
-      return defaultUser;
+      return {
+        ...defaultUser,
+        error,
+      };
     default:
       return state;
   }
