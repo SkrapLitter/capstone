fc539538ca54f46f6e2fc3e9350bf6bb462fdad2
Joshua Geisler <joshuageisler@Joshuas-MBP.home>
2020-09-04 11:25:41 -0400

    new job form validation working

--- src/components/jobComponents/createJob.tsx
+++ src/components/jobComponents/createJob.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { StoreState } from '../../store/store';
 import axios from 'axios';
@@ -8,19 +9,12 @@ import CreateAccountOverlay from '../userComponents/createAccountOverlay';
 const CreateJob: React.FC = () => {
   const selectUser = (state: StoreState) => state.user;
   const user = useSelector(selectUser);
-
+  const history = useHistory();
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [address, setAddress] = useState('');
   const [description, setDescription] = useState('');
   const [shouldShowCreateUser, setShouldShowCreateUser] = useState(false);
-
-  const setters: React.Dispatch<React.SetStateAction<string>>[] = [
-    setName,
-    setPrice,
-    setAddress,
-    setDescription,
-  ];
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -39,14 +33,40 @@ const CreateJob: React.FC = () => {
   };
 
   useEffect(() => {
+    // if user is logged in - hide create account overlay
     if (user.clearance) setShouldShowCreateUser(false);
   }, [user.clearance]);
 
-  const handleSubmit = () => {
+  const isValid = (): boolean => {
+    const form = document.getElementById('createJobForm');
+    const validFields = form.querySelectorAll('.valid');
+    // 2 inputs + textarea are valid and address object returned from Google
+    return validFields.length === 3 && !!address;
+  };
+
+  const handleSubmit = (): void => {
+    // validate form and highlight invalid fields
+    if (!isValid()) {
+      const form = document.getElementById('createJobForm');
+      const inputs = form.querySelectorAll('input');
+      const textarea = form.querySelector('textarea');
+      inputs.forEach(input => {
+        if (!input.classList.contains('valid')) {
+          input.classList.add('invalid');
+        }
+      });
+      if (!textarea.classList.contains('valid')) {
+        textarea.classList.add('invalid');
+      }
+      return;
+    }
+    // check if user is logged in
     if (!user.clearance) {
       setShouldShowCreateUser(true);
       return;
     }
+    // form is valid and user is logged in
+    // submit form
     axios
       .post('/api/jobs', {
         name,
@@ -55,16 +75,21 @@ const CreateJob: React.FC = () => {
         description,
         userId: user.id,
       })
-      .then(() => {
-        setters.forEach(fn => fn(''));
+      .then(({ data }) => {
+        // get jobId for navigation
+        const id = data.jobId;
+        // navigate to job details page
+        history.push(`/jobs/${id}`);
       })
+      // TODO - error handling for server errors - Toast?
       .catch(console.log);
   };
-  console.log('ADDRESS', address);
+
   return (
     <div
       className="container"
       style={{ maxWidth: '400px', textAlign: 'center' }}
+      id="createJobForm"
     >
       <h4>Create New Job</h4>
       <div className="input-field fsField">
@@ -72,6 +97,8 @@ const CreateJob: React.FC = () => {
           value={name}
           onChange={e => handleChange(e, setName, 'nameLabel')}
           id="name"
+          autoComplete="off"
+          className={name.length ? (name.length > 3 ? 'valid' : 'invalid') : ''}
         />
         <label htmlFor="name" id="nameLabel">
           Job Name
@@ -82,6 +109,7 @@ const CreateJob: React.FC = () => {
           value={price}
           onChange={e => handleChange(e, setPrice, 'priceLabel')}
           id="price"
+          autoComplete="off"
           className={
             price.length ? (/^[0-9]+$/.test(price) ? 'valid' : 'invalid') : ''
           }
@@ -99,10 +127,17 @@ const CreateJob: React.FC = () => {
       </div>
       <div className="input-field fsField">
         <textarea
-          className="createDescription"
           value={description}
           onChange={e => setDescription(e.target.value)}
           id="description"
+          autoComplete="off"
+          className={
+            description.length
+              ? description.length > 3
+                ? 'createDescription valid'
+                : 'createDescription invalid'
+              : 'createDescription'
+          }
         />
         <label htmlFor="description" id="descriptionLabel">
           Description
