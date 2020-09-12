9dbc7b3a92c8c4202c7b3d742692ab652cd99650
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-09-11 20:57:15 -0400

    Added tabs

--- src/components/accountComponent/editAccount.tsx
+++ src/components/accountComponent/editAccount.tsx
@@ -4,9 +4,17 @@ import { StoreState } from '../../store/store';
 import { validate } from '../validation';
 import { updateAccountThunk } from '../../store/user/userActions';
 
+import M from 'materialize-css';
+
 const EditAccount: React.FC = () => {
   const selectUser = (state: StoreState) => state.user;
   const user = useSelector(selectUser);
+
+  const selectJob = (state: StoreState) => state.job;
+  const job = useSelector(selectJob);
+
+  const selectInbox = (state: StoreState) => state.inbox;
+  const inbox = useSelector(selectInbox);
 
   const [username, setUsername] = useState(user.username);
   const [firstName, setFirstName] = useState(user.firstName);
@@ -18,6 +26,9 @@ const EditAccount: React.FC = () => {
     setUsername(user.username);
     setFirstName(user.firstName);
     setLastName(user.lastName);
+
+    const tabs = document.querySelector('.tabs');
+    M.Tabs.init(tabs);
   }, [user]);
 
   const toggleDisabled = (labelId: string): void => {
@@ -77,104 +88,137 @@ const EditAccount: React.FC = () => {
   };
   return (
     <>
-      <div className="container" id="accountForm">
-        <img
-          src={user.image}
-          width="50"
-          height="50"
-          className="border-circle"
-          alt={`${firstName} ${lastName}`}
-        />
-        <h4>
-          <strong>
-            {firstName} {lastName}
-          </strong>{' '}
-          Profile
-        </h4>
-        <div className="row">
-          <div className="col s11 input-field">
-            <input
-              autoComplete="off"
-              disabled
-              value={firstName}
-              onChange={e => handleChange(e, setFirstName, 'fnLabel')}
-              type="text"
-              className={validate.setClassName(validate.isName(firstName))}
-              id="firstName"
-            />
-            <label htmlFor="firstName" className="active" id="fnLabel">
-              First Name
-            </label>
-          </div>
-          <div className="col s1">
-            <button
-              onClick={() => toggleDisabled('firstName')}
-              className="btn-floating btn-small waves-effect waves-light"
-              type="submit"
-            >
-              <i className="material-icons">create</i>
-            </button>
+      <img
+        src={user.image}
+        width="50"
+        height="50"
+        className="border-circle"
+        alt={`${firstName} ${lastName}`}
+      />
+      <h4>
+        <strong>
+          {firstName} {lastName}
+        </strong>
+      </h4>
+      <div className="row">
+        <div className="col s12">
+          <ul className="tabs">
+            <li className="tab col s3">
+              <a href="#edit-profile" className="green-text text-accent-4">
+                Edit Profile
+              </a>
+            </li>
+            <li className="tab col s3">
+              <a href="#jobs" className="green-text text-accent-4">
+                My Jobs ({job.count || 0})
+              </a>
+            </li>
+            <li className="tab col s3">
+              <a href="#messages" className="green-text text-accent-4">
+                Messages ({inbox.inbox.length || 0})
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="edit-profile" className="col s12">
+          <div className="container m-t-l" id="accountForm">
+            <h2>Edit Profile</h2>
+            <div className="row">
+              <div className="col s11 input-field">
+                <input
+                  autoComplete="off"
+                  disabled
+                  value={firstName}
+                  onChange={e => handleChange(e, setFirstName, 'fnLabel')}
+                  type="text"
+                  className={validate.setClassName(validate.isName(firstName))}
+                  id="firstName"
+                />
+                <label htmlFor="firstName" className="active" id="fnLabel">
+                  First Name
+                </label>
+              </div>
+              <div className="col s1">
+                <button
+                  onClick={() => toggleDisabled('firstName')}
+                  className="btn-floating btn-small waves-effect waves-light"
+                  type="submit"
+                >
+                  <i className="material-icons">create</i>
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s11 input-field">
+                <input
+                  autoComplete="off"
+                  disabled
+                  value={lastName}
+                  onChange={e => handleChange(e, setLastName, 'lnLabel')}
+                  type="text"
+                  className={validate.setClassName(validate.isName(lastName))}
+                  id="lastName"
+                />
+                <label htmlFor="lastName" className="active" id="lnLabel">
+                  Last Name
+                </label>
+              </div>
+              <div className="col s1">
+                <button
+                  onClick={() => toggleDisabled('lastName')}
+                  className="btn-floating btn-small waves-effect waves-light"
+                  type="submit"
+                >
+                  <i className="material-icons">create</i>
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s11 input-field">
+                <input
+                  autoComplete="off"
+                  disabled
+                  value={username}
+                  onChange={e => handleChange(e, setUsername, 'emLabel')}
+                  type="email"
+                  className={validate.setClassName(validate.isEmail(username))}
+                  id="email"
+                />
+                <label htmlFor="email" className="active" id="emLabel">
+                  Email
+                </label>
+              </div>
+              <div className="col s1">
+                <button
+                  onClick={() => toggleDisabled('email')}
+                  className="btn-floating btn-small waves-effect waves-light"
+                  type="submit"
+                >
+                  <i className="material-icons">create</i>
+                </button>
+              </div>
+            </div>
+            <div className="center">
+              <button
+                onClick={handleSubmit}
+                className="btn waves-effect waves-light green accent-4"
+                type="submit"
+              >
+                Update Account
+                <i className="material-icons right">account_circle</i>
+              </button>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col s11 input-field">
-            <input
-              autoComplete="off"
-              disabled
-              value={lastName}
-              onChange={e => handleChange(e, setLastName, 'lnLabel')}
-              type="text"
-              className={validate.setClassName(validate.isName(lastName))}
-              id="lastName"
-            />
-            <label htmlFor="lastName" className="active" id="lnLabel">
-              Last Name
-            </label>
-          </div>
-          <div className="col s1">
-            <button
-              onClick={() => toggleDisabled('lastName')}
-              className="btn-floating btn-small waves-effect waves-light"
-              type="submit"
-            >
-              <i className="material-icons">create</i>
-            </button>
+        <div id="jobs" className="col s12">
+          <div className="m-t-l">
+            <h2>My Jobs</h2>
           </div>
         </div>
-        <div className="row">
-          <div className="col s11 input-field">
-            <input
-              autoComplete="off"
-              disabled
-              value={username}
-              onChange={e => handleChange(e, setUsername, 'emLabel')}
-              type="email"
-              className={validate.setClassName(validate.isEmail(username))}
-              id="email"
-            />
-            <label htmlFor="email" className="active" id="emLabel">
-              Email
-            </label>
+        <div id="messages" className="col s12">
+          <div className="m-t-l">
+            <h2>My Messages</h2>
           </div>
-          <div className="col s1">
-            <button
-              onClick={() => toggleDisabled('email')}
-              className="btn-floating btn-small waves-effect waves-light"
-              type="submit"
-            >
-              <i className="material-icons">create</i>
-            </button>
-          </div>
-        </div>
-        <div className="center">
-          <button
-            onClick={handleSubmit}
-            className="btn waves-effect waves-light green accent-4"
-            type="submit"
-          >
-            Update Account
-            <i className="material-icons right">account_circle</i>
-          </button>
         </div>
       </div>
     </>
