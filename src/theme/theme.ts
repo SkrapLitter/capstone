ebed8d589138e257491258aab2dd7bc630a99668
Joshua Geisler <joshuageisler@Joshuas-MBP.home>
2020-09-26 12:59:43 -0400

    lkjadfkjhasdlkjhsadf

---
+++ src/theme/theme.ts
@@ -0,0 +1,20 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#d7f4cb',
+      main: '#83e85a',
+      dark: '#055425',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#cce7ff',
+      main: '#369bf4',
+      dark: '0a1d2e',
+      contrastText: '#000',
+    },
+  },
+});
+
+export default theme;
