703d7fb88a74752ceb1677976b749e4830b850ef
Joshua Geisler <joshuageisler@Joshuas-MBP.home>
2020-09-28 10:32:59 -0400

    updated jobDetails page

--- src/components/jobDetailsComponent/posterButtons.tsx
+++ src/components/jobDetailsComponent/posterButtons.tsx
@@ -6,6 +6,8 @@ import { StoreState } from '../../store/store';
 import { findOrCreateChat } from '../../store/inbox/inboxActions';
 import { Chatroom } from '../../store/inbox/inboxInterface';
 import Button from '@material-ui/core/Button';
+import EditIcon from '@material-ui/icons/Edit';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 
 interface RouteParams {
   id: string;
@@ -57,9 +59,11 @@ const PosterButtons: React.FC = () => {
   return (
     <div style={{ display: 'flex' }}>
       <Button variant="outlined" onClick={openEditPage} className="m1em">
+        <EditIcon className="buttonIcon" />
         Edit Details
       </Button>
       <Button variant="outlined" onClick={openChat} className="m1em">
+        <MailOutlineIcon className="buttonIcon" />
         Message Poster
       </Button>
     </div>
