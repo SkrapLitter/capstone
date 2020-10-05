893c64f3f36d17e972ee363df02658a54ee523cd
nickcarr51 <nick.carr84@gmail.com>
2020-10-05 16:55:55 -0400

    fixed urls

--- src/socket.tsx
+++ src/socket.tsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 // const SOCKET_IO_URL = process.env.NODE_ENV
 //   ? 'http://skraplitter.herokuapp.com'
 //   : 'http://localhost:3000';
-const SOCKET_IO_URL = 'http://skraplitter.herokuapp.com';
+const SOCKET_IO_URL = 'https://skraplitter.herokuapp.com';
 const socket = io(SOCKET_IO_URL);
 
 export default socket;
