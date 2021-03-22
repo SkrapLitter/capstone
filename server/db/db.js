e18270fdc0438f39173e9fcc299939b69a18b3f0
nickcarr51 <nick.carr84@gmail.com>
2021-03-22 12:48:17 -0400

    updated packages, updated DB config

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,30 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/capstone2020',
-  {
+const DATABASE_URL =
+  process.env.DATABASE_URL || 'postgres://localhost:5432/capstone2020';
+
+let config;
+if (process.env.DATABASE_URL) {
+  config = {
     logging: false,
-  }
-);
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+  };
+}
+
+const db = new Sequelize(DATABASE_URL, config);
 
 module.exports = db;
